b78b8794d5f90e1f3e136468c751304fb2b8281f
svntifernvndez <101257505+svntifernvndez@users.noreply.github.com>
2022-05-23 14:11:25 +0200

    sdfs

--- Escenes/Escena final.gd
+++ Escenes/Escena final.gd
@@ -9,3 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$win.play()
+	
+func _process(delta):
+	if Input.is_action_pressed("R"):
+		get_tree().change_scene("res://Escenes/pantallainici.tscn")
